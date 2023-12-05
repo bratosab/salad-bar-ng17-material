bf9a6692cb6569576f371c213fc2854100458dd0
Abdallah <bratosab@gmail.com>
2023-12-05 09:18:51 +0100

    Day 1 - app structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'kitchen',
+    loadChildren: () => import('./kitchen/kitchen.module').then(m => m.KitchenModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
